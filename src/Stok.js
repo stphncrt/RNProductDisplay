94b7b4823f2dfe7a1fee577afb0204f35246580d
stphncrt <cretestephen@gmail.com>
2021-03-03 19:32:00 +0300

    searchbar added

--- src/Stok.js
+++ src/Stok.js
@@ -1,13 +1,24 @@
-import React from 'react';
-import {View, Text, FlatList} from 'react-native';
+import React, {useEffect, useState} from 'react';
+import {View, Text, FlatList, StyleSheet, Alert, TextInput} from 'react-native';
 import productData from './products.json';
 import {ProductCard} from './components/ProductCard';
 
 const Stok = () => {
+  const [searchValue, setSearchValue] = useState('');
   const renderListItem = ({item}) => <ProductCard product={item} />;
 
+  // useEffect(() => {
+  //   Alert.alert('Clarushop', 'Wellcome, Enjoy your shopping..');
+  // }, []);
   return (
     <View>
+      <Text style={styles.banner}>ClaruShop</Text>
+      <View style={styles.searchBar}>
+        <TextInput
+          placeholder={'Search your product...'}
+          onChangeText={(value) => setSearchValue(value)}
+        />
+      </View>
       <FlatList
         keyExtractor={(item, index) => index.toString()}
         data={productData}
@@ -19,3 +30,18 @@ const Stok = () => {
 };
 
 export default Stok;
+
+const styles = StyleSheet.create({
+  banner: {
+    textAlign: 'center',
+    color: 'purple',
+    fontWeight: 'bold',
+    fontSize: 40,
+  },
+  searchBar: {
+    backgroundColor: '#eceff1',
+    padding: 5,
+    margin: 10,
+    borderRadius: 10,
+  },
+});
